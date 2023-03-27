b35da13752b43f2f5c02fe0068bff807cbfdd0f4
SangNX <matterhorn@generative.xyz>
2023-03-27 18:48:27 +0700

    fix: add web3-provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import routes from '@/routes/index';
 import { useRoutes } from 'react-router-dom';
+import Web3Provider from '@/components/Web3Provider';
+import { Provider } from 'react-redux';
+import store from '@/state';
+import { WalletProvider } from '@/contexts/wallet-context';
 
 const App = () => {
   const element = useRoutes(routes);
-
-  return <>{element}</>;
+  return (
+    <Provider store={store}>
+      <Web3Provider>
+        <WalletProvider>{element}</WalletProvider>
+      </Web3Provider>
+    </Provider>
+  );
 };
 
 export default App;
